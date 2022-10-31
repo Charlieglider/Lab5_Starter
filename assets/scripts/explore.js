59d0460f9eca25e1df37c430a427a2827fad01d8
Charlieglider <arbeit19@gmail.com>
2022-10-30 20:20:21 -0700

    added explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,62 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var button = document.querySelector("button");
+  var img = document.querySelector("img");
+  var text = document.getElementById("text-to-speak");
+  var talk = new SpeechSynthesisUtterance(" ");
+
+  if(text.addEventListener) {
+    text.addEventListener('input', (event) =>{
+      talk= new SpeechSynthesisUtterance(text.value);
+    }, false);
+  }
+  else if (text.attachEvent) {
+    text.attachEvent('onpropertychange', function(){
+
+    });
+  }
+  const synth = window.speechSynthesis;
+  const inputTxt = document.querySelector('.txt');
+  const voiceSelect = document.querySelectorAll('select');
+  let voices = [];
+
+
+  function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length ; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += ' — DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+button.addEventListener('click', (event) =>{
+  const choice = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+  for (let i=0; i < voices.length; i++){
+    if (voices[i].name === choice) {
+      talk.voice = voices[i];
+    }
+  }
+  synth.speak(talk);
+  img.src = "assets/images/smiling-open.png"
+  talk.addEventListener('end', function(event) {
+      img.src = "assets/images/smiling.png"
+    });
+
+});
 }
